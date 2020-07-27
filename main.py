c5bcf028fb2e7e50ec9bdcaba47eb5e61049d6fc
Sanaa514 <sanaamontgomery10@gmail.com>
2020-07-27 15:36:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+def foo(first, second, third, therest):
+    print("First: Sanaa" ,first)
+    print("Second: May", second)
+    print("Third: 14th" ,third)
+    print("And all the rest... 2004" )
+
+foo(First, Second, Third, And all the rest... )
